6d4993a62fbe348aff80d371c47cbc7512670f4f
DajerlyMoreno <dajerlymoga@gmail.com>
2023-11-10 12:07:05 -0500

    añadi ejercicio 1 al menu

--- MainMenu.c
+++ MainMenu.c
@@ -1,9 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "romanNumeral.c"
+
+void menu();
+void romanNumber();
 
 int main() {
 
-    printf("Hola");
-
+    menu();
 
     return 0;
+}
+
+void menu(){
+    char optionMenu;
+    do{
+        printf("Ingrese el indice de la accion que desea realizar\n1. Numero Romano\n0. Salir");
+        optionMenu = getchar();
+        switch (optionMenu){
+        case '1':
+            romanNumber();
+            break;
+        
+        default:
+            break;
+        }
+    }while(optionMenu != '0');
+}
+
+void romanNumber(){
+    int number;
+    char out;
+    do{
+        printf("Ingrese un numero: ");
+        scanf("%d", &number);
+        printf("%s\n", romanNumeral(number));
+        printf("Para regresar almenu principal digite 1, para convertir otro numero digite cualquier techa:  ");
+        scanf("%c", &out);
+        scanf("%c", &out);
+
+    }while(out != '1');
+    
 }
